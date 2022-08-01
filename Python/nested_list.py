b0095df1a957f356380a22c9c0672ace34250898
sonjake2020 <sonjake2020@gmail.com>
2022-08-01 10:01:22 +0700

    find the percentage

--- Python/nested_list.py
+++ Python/nested_list.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
         name = input()
         score = float(input())
         records.append([name, score])
-    
     lowest_score = 100
     sec_lowest_score = 100
     for record in records:
@@ -13,13 +12,10 @@ if __name__ == '__main__':
     student = []    
     for record in records:
         if record[1] < sec_lowest_score and record[1] > lowest_score:
-            sec_lowest_score = record[1]
-            
+            sec_lowest_score = record[1]   
     for record in records:
         if record[1] == sec_lowest_score:
             student.append(record[0])
-    
-    
     student.sort()
     for i in student:
         print(i)    
